66a56d8cc6e5987d362af4f8534d4324f63f2e8f
Dizzyme12 <73571301+Dizzyme12@users.noreply.github.com>
2021-06-30 11:43:45 +0700

    Update ConnectionSQL.java

--- OnlineShop/src/onlineshop/ConnectionSQL.java
+++ OnlineShop/src/onlineshop/ConnectionSQL.java
@@ -6,8 +6,9 @@ public class ConnectionSQL {
     java.sql.Connection conn;
     public java.sql.Connection getConSQL(){
         try{
+            // using SQL Management Studio
             String url = "jdbc:sqlserver://localhost:1433;"
-                    + "databaseName=db_olshop;user=sa;password=shandy122002";
+                    + "databaseName=db_olshop;user=yourusername;password=yourpassword";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             conn = DriverManager.getConnection(url);
         }
